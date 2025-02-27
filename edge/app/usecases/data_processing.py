7dd0d2df3914954af598025522062e190a91a034
Hrystyna Yaskulska <yaskulskahrystyna@gmail.com>
2025-02-27 09:17:10 +0200

    Implement pothole detection in data processing

--- edge/app/usecases/data_processing.py
+++ edge/app/usecases/data_processing.py
@@ -1,6 +1,9 @@
+from collections import deque
 from app.entities.agent_data import AgentData
 from app.entities.processed_agent_data import ProcessedAgentData
 
+z_history = deque(maxlen=5)
+THRESHOLD = 2000
 
 def process_agent_data(
     agent_data: AgentData,
@@ -13,9 +16,19 @@ def process_agent_data(
         processed_data_batch (ProcessedAgentData): Processed data containing the classified state of the road surface and agent data.
     """
 
+    z_value = agent_data.accelerometer.z
+    prev_z = z_history[-1] if z_history else z_value
+
+    z_history.append(z_value)
+
+    z_diff = prev_z - z_value
+    is_pothole = z_diff > THRESHOLD
+
+    road_state = "rough" if is_pothole else "smooth"
+
     processed_data = ProcessedAgentData(
-        road_state="default",
+        road_state=road_state,
         agent_data=agent_data
     )
-    
+
     return processed_data
